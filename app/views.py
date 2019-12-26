a2484996179cbf9ad8fa32d223e36e05e33d57bf
Brian-M-Code <mutuma.brian@yahoo.com>
2019-12-26 20:36:01 +0300

    Added title of the app

--- app/views.py
+++ app/views.py
@@ -9,8 +9,8 @@ def index():
     View root page function that returns the index page and its data
     '''
     
-    message = 'News World'
-    return render_template('index.html', message = message)
+    title = 'News World - Welcome to the Best News website Online'
+    return render_template('index.html', title = title)
 
 @app.route('/news/<news_id>')
 def news(news_id):
