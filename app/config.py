b0fcc5ab9304abf08eb6b1f84c2faa51621bb1c9
Brian-M-Code <mutuma.brian@yahoo.com>
2019-12-28 11:16:52 +0300

    added get news function

--- app/config.py
+++ app/config.py
@@ -3,7 +3,7 @@ class Config:
     General configuration parent class
     '''
     
-    NEWS_API_BASE_URL ='https://newsapi.org/v2/top-headlines?' 'country= ke&/{}?api_key={}'
+    NEWS_API_BASE_URL ='https://newsapi.org/v2/top-headlines?country={}&api_key={}'
     pass
 
 
