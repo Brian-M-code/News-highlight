e20b7ecd4df3978ab67eb6c88c5dbdd900aa1829
Brian-M-Code <mutuma.brian@yahoo.com>
2020-01-02 07:24:21 +0300

    added bootstrap and a navbar in the base html file

--- app/__init__.py
+++ app/__init__.py
@@ -9,3 +9,12 @@ app.config.from_object(DevConfig)
 app.config.from_pyfile('config.py')
 
 from app import views
+
+from flask_bootstrap import Bootstrap
+
+#setting up congigurations
+app.config.from_object(DevConfig)
+app.config.from_pyfile("config.py")
+
+#initializing flask bootstarp extension
+bootstrap = Bootstrap(app)
